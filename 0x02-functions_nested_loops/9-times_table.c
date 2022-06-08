d0c5e6c60f2f06a95a27d652445a75c0a7e040a7
nsikak02 <nsikakdbest@gmail.com>
2022-06-07 23:52:09 -0700

    nine times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,7 @@ int main()
 int n, i;
 printf("Enter an integer: ");
 scanf("%d", &n);
-for (i = 1; i <= 10; ++i) {
+for (i = 0; i <= 10; ++i) {
 printf("%d * %d = %d \n", n, i, n * i);
 }
 return 0;
