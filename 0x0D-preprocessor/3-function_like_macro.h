4cfa9714cb60db20447faafca0a87b0286e9c1e9
nsikak02 <101870931+nsikak02@users.noreply.github.com>
2022-07-05 00:39:02 +0100

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,22 +1,3 @@
-#include <stdio.h>
-
-/**
-* main - Entry point
-*
-* Description: prints the name of the file it was
-*              compiled from, followed by a new line
-*
-* Return: Always 0 (Success)
-*/
-
-int main(void)
-{
-printf("%s\n", __FILE__);
-return (0);
-}
-root@29cb72c3ab23:/Norbert/alx-low_level_programming/0x0D-preprocessor# ^C
-root@29cb72c3ab23:/Norbert/alx-low_level_programming/0x0D-preprocessor# ^C
-root@29cb72c3ab23:/Norbert/alx-low_level_programming/0x0D-preprocessor# cat 3-function_like_macro.h
 #ifndef _FUNCTION_LIKE_MACRO_H
 #define _FUNCTION_LIKE_MACRO_H
 
