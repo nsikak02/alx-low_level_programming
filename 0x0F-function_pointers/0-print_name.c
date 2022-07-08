ae2382b5b7c79cc80a65745f936329e1ce0b792e
nsikak02 <101870931+nsikak02@users.noreply.github.com>
2022-07-08 05:38:27 +0100

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,1 +1,14 @@
-hgdgf
+/**
+* print_name - function to print name
+*
+* @name: name to print
+* @f: function pointer that doesn't return anything
+*
+* Return: Always 0 (Success)
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+if (name && f)
+f(name);
+}
