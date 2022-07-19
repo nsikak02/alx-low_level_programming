48f63a1afe02c1d39fe3e1ae4b2d0dd216c1e94b
nsikak02 <101870931+nsikak02@users.noreply.github.com>
2022-07-19 04:11:20 +0100

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,23 +9,23 @@
 */
 void free_listint2(listint_t **head)
 {
-        listint_t *current, *temp;
+listint_t *current, *temp;
 
-        if (head != NULL)
-        {
-                /*set head addr to current*/
-                current = *head;
+if (head != NULL)
+{
+/*set head addr to current*/
+current = *head;
 
-                /*iterate through the whole list*/
-                /*while setting the current node to temp*/
-                while ((temp = current) != NULL)
-                {
-                        /*set next node to curretnt*/
-                        current = current->next;
-                        /*free temp, that is the current node*/
-                        free(temp);
-                }
+/*iterate through the whole list*/
+/*while setting the current node to temp*/
+while ((temp = current) != NULL)
+{
+/*set next node to curretnt*/
+current = current->next;
+/*free temp, that is the current node*/
+free(temp);
+}
 
-                *head = NULL;
-        }
+*head = NULL;
+}
 }
