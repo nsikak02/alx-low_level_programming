6b3ae0cac22b79d28c7f661a6949807640922d82
nsikak02 <101870931+nsikak02@users.noreply.github.com>
2022-07-19 04:07:19 +0100

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,13 +9,13 @@
 */
 void free_listint(listint_t *head)
 {
-        listint_t *current;
+listint_t *current;
 
-        /*iterate through each node in list*/
-        while ((current = head) != NULL)
-        {
-                /*set head as the next node*/
-                head = head->next;
-                free(current);
-        }
+/*iterate through each node in list*/
+while ((current = head) != NULL)
+{
+/*set head as the next node*/
+head = head->next;
+free(current);
+}
 }
