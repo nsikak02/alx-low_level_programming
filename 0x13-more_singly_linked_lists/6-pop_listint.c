4bce31246cd05595ebbdea29b4406401a0c267d7
nsikak02 <101870931+nsikak02@users.noreply.github.com>
2022-07-19 04:12:56 +0100

    Update 6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,26 +9,26 @@
 */
 int pop_listint(listint_t **head)
 {
-        int first_node;
-        listint_t *temp, *next;
+int first_node;
+listint_t *temp, *next;
 
-        /*if list is empty return 0*/
-        if (*head == NULL)
-                return (0);
+/*if list is empty return 0*/
+if (*head == NULL)
+return (0);
 
-        /*set head addr to temp*/
-        temp = *head;
-        /*get addr of next node*/
-        next = temp->next;
-        /*get element of first node*/
-        first_node = temp->n;
+/*set head addr to temp*/
+temp = *head;
+/*get addr of next node*/
+next = temp->next;
+/*get element of first node*/
+first_node = temp->n;
 
-        /*free first node*/
-        free(temp);
+/*free first node*/
+free(temp);
 
-        /*set head to second node*/
-        *head = next;
+/*set head to second node*/
+*head = next;
 
-        /*return element of first node*/
-        return (first_node);
+/*return element of first node*/
+return (first_node);
 }
